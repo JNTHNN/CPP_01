bdd47f242190fc6368d6ca380018f6787ce0ad55
JNTHNN <j.gasparoly@gmail.com>
2024-11-27 10:09:58 +0100

    done: ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgasparo <jgasparo@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 19:41:42 by jgasparo          #+#    #+#             */
+/*   Updated: 2024/11/27 10:08:18 by jgasparo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str("HI THIS IS BRAIN");
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+
+	std::cout << "STRING	ADDRESS : " << &str << std::endl;
+	std::cout << "STRINGPTR	ADDRESS : " << &stringPTR << std::endl;
+	std::cout << "STRINGREF	ADDRESS : " << &stringREF << std::endl;
+
+	std::cout << "---------------------" << std::endl;
+	
+	std::cout << "STRING	VALUE : " << str << std::endl;
+	std::cout << "STRINGPTR	VALUE : " << *stringPTR << std::endl;
+	std::cout << "STRINGREF	VALUE : " << stringREF << std::endl;
+
+	return EXIT_SUCCESS;
+}
